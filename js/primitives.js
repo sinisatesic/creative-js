12edf5fd213b9163cbddcc536b8a099b0fbd8265
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-04-08 08:54:52 -0500

    adding primitives to creative-js

--- js/primitives.js
+++ js/primitives.js
@@ -1,13 +1,26 @@
 //Primitive Data Types
 
+
+
 //Numbers
+const day = 5;
+console.log(typeof day); //will return number
 
 //Strings
+const name = "Dev Ed";
+console.log(typeof name); //returns string
 
 //Booleans
+const isLogged = false;
+console.log(typeof isLogged);
 
 //Undefined
+let willBeUndefined;
+console.log(willBeUndefined); //undefined ; variable that has no value assigned to it
 
 //Null
+let minute = null; //explicitly telling JS that value is nothing
+console.log(minute);
 
 //Symbol
+const symbol = Symbol();
