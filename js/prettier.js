eb814f26e0860d215c764836184f6157bec90ba4
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-04-09 13:48:49 -0500

    prettier

---
+++ js/prettier.js
@@ -0,0 +1,9 @@
+const bank1 = 100;
+const bank2 = 50;
+
+const allBanks = bank1 + bank2;
+const user = "Ed";
+
+const greeting = `Hey there welcome to our ${user}`;
+
+//this lesson is over an extension on visual studio code; Prettier - Code Formatter
