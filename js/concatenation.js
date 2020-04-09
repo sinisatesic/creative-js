1d16be06380d341270a0bdc07682a96075455f58
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-04-09 13:46:14 -0500

    concatenation

---
+++ js/concatenation.js
@@ -0,0 +1,19 @@
+const bank1 = 100;
+const bank2 = 50;
+
+const allBanks = bank1 + bank2;
+
+console.log(allBanks); //150
+
+const greeting = 'Welcome to da it\'s website "BLORKLAND" ';
+const userName = " - Blark";
+console.log(greeting + " " + userName);
+
+//BACKTICKS
+const user = "Ed";
+const greeting = `Hey there it's ya boi "lala"`;
+
+
+
+
+const secondGreeting = `Hey there, welcome to our ${user}`;
