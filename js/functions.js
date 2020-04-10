69a4dda267710f3bda6cfcb4295d832f4f7a60ff
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-04-10 00:07:58 -0500

    functions

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+
+function greet(){
+    let target = "poopboi";
+    let name = prompt(`What ya name, ${target}?`);
+    console.log(`Welcome, ${name}`);
+}
+    console.log(`rest of code`);
+
+
+function signUP(){
+    greet();
+}
+
+signUP();
+
+
