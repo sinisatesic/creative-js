d7863b326664bd5c7b6885f9e50c13ad72eec449
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-06-13 22:44:14 -0500

    js practice

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -1201,6 +1201,22 @@ console.log(isHighest([1, 2, 3, 4]));
 
 
 
+for(let i = 1; i <= 100; i++){
+    if (i % 3 == 0) console.log('Fizz');
+    else if (i % 5 == 0) console.log('Buzz');
+    else if (i % 15 == 0) console.log('FizzBuzz');
+    else console.log(i);
+}
+
+
+const isHighest = (arr) => {
+    let highest = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] < highest) highest = arr[i];
+    }
+    return highest;
+};
+isHighest([1,2,3,4]);
 
 
 
