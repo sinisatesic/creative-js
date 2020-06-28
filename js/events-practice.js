c0c7dc4e3fd67b4ff8a8d5d69e005b486e1fdcb1
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-06-28 17:59:03 -0500

    more events practice

--- js/events-practice.js
+++ js/events-practice.js
@@ -16,6 +16,58 @@ third.addEventListener('mouseenter', e => {
 
 //dynamic adding below
 
+// const button = document.querySelector('.submit');
+// const list = document.querySelector('.list');
+// const tasks = list.children;
+// const numTasks = document.querySelector('.numTasks b');
+//
+// button.addEventListener('click', e => {
+//     const newTask = document.createElement('li');
+//     newTask.classList.add('task');
+//     newTask.innerText = `task ${tasks.length + 1}`;
+//     list.appendChild(newTask);
+//     numTasks.innerText = tasks.length;
+// });
+
+// const button = document.querySelector('.submit');
+// const list = document.querySelector('.list');
+// const numTasks = document.querySelector('.numTasks b');
+// const tasks = list.children;
+//
+// button.addEventListener('click', e => {
+//     const newTask = document.createElement('li');
+//     newTask.classList.add('task');
+//     newTask.innerText = `task ${tasks.length + 1}`
+//     list.appendChild(newTask);
+//     numTasks.innerText = tasks.length;
+// });
+
+// const button = document.querySelector('.submit');
+// const list = document.querySelector('.list');
+// const tasks = list.children;
+// const numTasks = document.querySelector('.numTasks b');
+//
+// button.addEventListener('click', e => { //event for click on submit button
+//     const newTask = document.createElement('li');   //create new li element
+//     newTask.classList.add('.task'); //add class of task to newly created li element
+//     newTask.innerText = `task ${tasks.length + 1}`; //what text will show after "task" for new li
+//     list.appendChild(newTask); //append newly created li element to ul with class list
+//     numTasks.innerText = tasks.length;  //number of tasks heading will contain text of the length of li tasks in ul with class list
+// });
+
+// const button = document.querySelector('.submit');
+// const list = document.querySelector('.list');
+// const tasks = list.children;
+// const numTasks = document.querySelector('.numTasks b');
+//
+// button.addEventListener('click', e => {
+//     const newTask = document.createElement('li');
+//     newTask.classList.add('task');
+//     newTask.innerText = `task ${tasks.length + 1}`;
+//     list.appendChild(newTask);
+//     numTasks.innerText = tasks.length;
+// });
+
 const button = document.querySelector('.submit');
 const list = document.querySelector('.list');
 const tasks = list.children;
