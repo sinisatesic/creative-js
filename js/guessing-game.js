b9a524ad1be76818eb11632c49bbfc2adbeadae7
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-06-29 22:35:09 -0500

    js practice

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -1306,17 +1306,83 @@
 // }
 // console.log(rev('hello there'));
 
-function fruit(name,color,shape){
+// function fruit(name,color,shape){
+//     this.name = name;
+//     this.color = color;
+//     this.shape = shape;
+//
+//     this.describe = function(){
+//         return 'A '+this.name+' is the color '+this.color+' and is the shape '+this.shape;
+//     }
+// }
+// let melon = new fruit('melon','green','round');
+// console.log(melon.describe());
+//
+// const isHighest = (arr) => {
+//     let highest = 0;
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] > highest){
+//             highest = arr[i];
+//         }
+//     }
+//     return highest;
+// };
+//
+// const fib = () => {
+//     let arr = [];
+//     for(let i = 0; i <= 10; i++){
+//         if(i == 0 || i == 1){
+//             arr.push(i);
+//         } else {
+//         arr.push(arr[i - 1] + arr[i - 2]);
+//         }
+//     }
+//     return arr;
+// };
+// console.log(fib());
+//
+// for(let i = 1; i <= 100; i++){
+//     if(i % 15 == 0) console.log('FizzBuzz');
+//     else if(i % 3 == 0) console.log('Fizz');
+//     else if(i % 5 == 0) console.log('Buzz');
+//     else console.log(i);
+// }
+//
+// const isPal = (string) => {
+//     let regular = string;
+//     let reverse = string.split('').reverse().join('');
+//     return regular === reverse;
+// };
+// console.log(isPal('ahoy'));
+
+function fruit(name, color, shape){
     this.name = name;
     this.color = color;
     this.shape = shape;
-
     this.describe = function(){
         return 'A '+this.name+' is the color '+this.color+' and is the shape '+this.shape;
     }
 }
 let melon = new fruit('melon','green','round');
 console.log(melon.describe());
+
+const fib = () => {
+    let arr = [];
+    for(let i = 0; i <= 10; i++){
+        if(i == 0 || i == 1){
+            arr.push(i);
+        } else {
+            arr.push(arr[i - 1] + arr[i - 2]);
+        }
+    }
+    return arr;
+};
+
+const isPal = (string) => {
+    let reg = string;
+    let rev = reg.split('').reverse().join('');
+    return reg === rev;
+}
 
 const isHighest = (arr) => {
     let highest = 0;
@@ -1328,30 +1394,10 @@ const isHighest = (arr) => {
     return highest;
 };
 
-const fib = () => {
-    let arr = [];
-    for(let i = 0; i <= 10; i++){
-        if(i == 0 || i == 1){
-            arr.push(i);
-        } else {
-        arr.push(arr[i - 1] + arr[i - 2]);
-        }
-    }
-    return arr;
-};
-console.log(fib());
-
 for(let i = 1; i <= 100; i++){
     if(i % 15 == 0) console.log('FizzBuzz');
-    else if(i % 3 == 0) console.log('Fizz');
+    else if(i % 3 == 0) console.log('FIzz');
     else if(i % 5 == 0) console.log('Buzz');
     else console.log(i);
 }
-
-const isPal = (string) => {
-    let regular = string;
-    let reverse = string.split('').reverse().join('');
-    return regular === reverse;
-};
-console.log(isPal('ahoy'));
 
