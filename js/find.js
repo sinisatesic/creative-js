2fc30238214161dcc6e39854e66ff09db44c3495
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-07-13 12:45:42 -0500

    js find lesson

---
+++ js/find.js
@@ -0,0 +1,9 @@
+//find will go through each element in array, and once it finds sought for element, will return just that element
+
+const videos = ['Star Wars IV', 'Star Wars V', 'Star Wars VI'];
+
+const searchForANewHope = videos.find(function(video){
+    return video.includes('IV');//will only return SW IV
+});
+
+//overall, find will retrieve back specific element from array
