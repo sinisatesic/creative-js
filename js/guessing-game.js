69d4ef5a928ae5bcb1e48d65ff740b116454447b
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-07-23 23:59:59 -0500

    guess with e6 but infinite loop - keep looking into it

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -1530,49 +1530,64 @@
 // }
 // guessGame();
 
-const isPal = (string) => {
-    let reg = string;
-    let rev = string.split('').reverse().join('');
-    return reg === rev;
-};
+// const isPal = (string) => {
+//     let reg = string;
+//     let rev = string.split('').reverse().join('');
+//     return reg === rev;
+// };
+//
+// const isFib = () => {
+//     let arr = [];
+//     for(let i = 0; i <= 10; i++){
+//         if(i == 0 || i == 1){
+//             arr.push(i);
+//         } else {
+//             arr.push(arr[i - 1] + arr[i - 2]);
+//         }
+//     }
+//     return arr;
+// };
+//
+// const isHighest = (arr) => {
+//     let highest = 0;
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] > highest){
+//             highest = arr[i];
+//         }
+//     }
+//     return highest;
+// };
+//
+// function fruit(name, color, shape){
+//     this.name = name;
+//     this.color = color;
+//     this.shape = shape;
+//     this.describe = function(){
+//         return 'A '+this.name+' is the color '+this.color+' and is the shape '+this.shape;
+//     }
+// }
+// let melon = new fruit('melon','green','round');
+// console.log(melon.describe());
+//
+// for(let i = 1; i <= 100; i++){
+//     if(i % 15 === 0) console.log('FizzBuzz');
+//     else if(i % 3 === 0) console.log('Fizz');
+//     else if(i % 5 === 0) console.log('Buzz');
+//     else console.log(i);
+// }
 
-const isFib = () => {
-    let arr = [];
-    for(let i = 0; i <= 10; i++){
-        if(i == 0 || i == 1){
-            arr.push(i);
-        } else {
-            arr.push(arr[i - 1] + arr[i - 2]);
+const guessGame = () => {
+    let guess;
+    let randomNum = Math.floor(Math.random() * 11);
+    guess = prompt('pick numb from 1 to 10');
+    do {
+        if(guess < randomNum){
+            console.log('num too low');
+        }else if(guess > randomNum){
+            console.log('num too high');
         }
-    }
-    return arr;
+    } while (guess !== randomNum);
+    console.log('ya did it');
 };
-
-const isHighest = (arr) => {
-    let highest = 0;
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] > highest){
-            highest = arr[i];
-        }
-    }
-    return highest;
-};
-
-function fruit(name, color, shape){
-    this.name = name;
-    this.color = color;
-    this.shape = shape;
-    this.describe = function(){
-        return 'A '+this.name+' is the color '+this.color+' and is the shape '+this.shape;
-    }
-}
-let melon = new fruit('melon','green','round');
-console.log(melon.describe());
-
-for(let i = 1; i <= 100; i++){
-    if(i % 15 === 0) console.log('FizzBuzz');
-    else if(i % 3 === 0) console.log('Fizz');
-    else if(i % 5 === 0) console.log('Buzz');
-    else console.log(i);
-}
+guessGame();
 
