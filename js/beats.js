d68c94076303ad4176b9813a1d1132d5b5b3919d
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-08-03 20:44:11 -0500

    added conditional logic for individual pad sounds, as well as resetting current time so audio's don't overlap

--- js/beats.js
+++ js/beats.js
@@ -25,7 +25,42 @@ class Kit {
         let step = this.index % 8; //from 0 index to last (8th) pad
         // console.log(`step: ${step} and index: ${this.index}`);
         const activeBars = document.querySelectorAll(`.b${step}`); //interpolates from each 'track' for their b0 to b7 classes
-        this.index++; //with above log, one will see logs of 0 - 7 via 1 sec increments
+        activeBars.forEach(bar => {
+            bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+            if(bar.classList.contains('active')){ //check if pads are active; if so, play sound
+                //but then, must check what sound to play; check each sound:
+                if(bar.classList.contains('kick-pad')){
+                    this.kickAudio.currentTime = 0; //set current time of this audio back to 0; each time sound is played
+                    this.kickAudio.play();
+                    // this.classList.toggle('jump');
+                }
+                if(bar.classList.contains('snare-pad')){
+                    this.snareAudio.currentTime = 0;
+                    this.snareAudio.play();
+                }
+                if(bar.classList.contains('hihat-pad')){
+                    this.hihatAudio.currentTime = 0;
+                    this.hihatAudio.play();
+                }
+                if(bar.classList.contains('perc-pad')){
+                    this.percAudio.currentTime = 0;
+                    this.percAudio.play();
+                }
+                if(bar.classList.contains('clap-pad')){
+                    this.clapAudio.currentTime = 0;
+                    this.clapAudio.play();
+                }
+                if(bar.classList.contains('tom-pad')){
+                    this.tomAudio.currentTime = 0;
+                    this.tomAudio.play();
+                }
+                if(bar.classList.contains('horn-pad')){
+                    this.hornAudio.currentTime = 0;
+                    this.hornAudio.play();
+                }
+            }
+        });
+        this.index++; //with above log, will see logs of 0 - 7 via 1 sec increments
     }
 
     start() {
@@ -40,6 +75,9 @@ const drumKit = new Kit();
 
 drumKit.pads.forEach(pad => {
     pad.addEventListener('click', drumKit.activePad);
+    pad.addEventListener('animationend', function(){
+        this.style.animation = ""; //here, keyword this refers to actual pad
+    }); //once animation finishes, its removed then added again
 });
 
 drumKit.playBtn.addEventListener('click', function() {
