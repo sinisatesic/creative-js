ff699004da460fb9d937ad709484fadbb3346d13
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-08-17 05:58:16 -0500

    git open and close panel adjustements/functionality

--- js/colors.js
+++ js/colors.js
@@ -5,6 +5,9 @@ const generateBtn = document.querySelector('.generate');
 const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll('.color h2');
 const popup = document.querySelector('.copy-container');
+const adjustButton = document.querySelectorAll('.adjust');
+const closeAdjustments = document.querySelectorAll('.close-adjustment');
+const sliderContainers = document.querySelectorAll('.sliders');
 let initialColors;
 
 //Event listeners
@@ -29,6 +32,18 @@ popup.addEventListener('transitionend', () => {
     const popupBox = popup.children[0];
     popup.classList.remove('active');
     popupBox.classList.remove('active');
+});
+
+adjustButton.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        openAdjustmentPanel(index);
+    });
+});
+
+closeAdjustments.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        closeAdjustmentPanel(index);
+    });
 });
 
 //Functions
@@ -178,4 +193,13 @@ function copyToClipboard(hex){
     popupBox.classList.add('active');
 }
 
+function openAdjustmentPanel(index){
+    sliderContainers[index].classList.toggle('active');
+}
+function closeAdjustmentPanel(index){
+    sliderContainers[index].classList.remove('active');
+}
+
 randomColors();
+
+
