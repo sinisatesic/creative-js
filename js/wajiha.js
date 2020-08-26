4b653b462f6a8477fd6d107383155aadd55eac5b
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-08-25 21:56:35 -0500

    updated css/js

--- js/wajiha.js
+++ js/wajiha.js
@@ -1,43 +1,86 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+// var slideIndex = 1;
+// showSlides(slideIndex);
+//
+// // Next/previous controls
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);
+// }
+//
+// // Thumbnail image controls
+// function currentSlide(n) {
+//     showSlides(slideIndex = n);
+// }
+//
+// function showSlides(n) {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+//     var dots = document.getElementsByClassName("dot");
+//     if (n > slides.length) {slideIndex = 1}
+//     if (n < 1) {slideIndex = slides.length}
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex-1].style.display = "block";
+//     dots[slideIndex-1].className += " active";
+// }
+//
+//
+//
+// const cards = document.querySelectorAll('.card');
+//
+// cards.addEventListener('hover', () => {
+//     enlargeCards();
+// });
+//
+// function enlargeCards(){
+//     const cards = document.querySelectorAll('.card');
+//     cards.forEach(card => {
+//         card.classList.add('cardy');
+//     });
+// }
 
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
 
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+//GSAP:
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-}
+var logo = new TimelineMax();
+var navbar = new TimelineMax();
+var screen = new TimelineMax();
 
+logo.from(".logo", 3, {
+    delay: 0.2,
+    opacity: 0,
+    x: -80,
+    ease: Expo.easeInOut
+}).from(".logo", 3, {
+    delay: 0.2,
+    opacity: 0,
+    x: -80,
+    ease: Expo.easeInOut
+}, "-=5");
 
+navbar.from(".navbar", 3, {
+    delay: 0.2,
+    opacity: 0,
+    y: -40,
+    ease: Expo.easeInOut
+}).from(".navbar", 3, {
+    delay: 0.2,
+    opacity: 0,
+    y: -40,
+    ease: Expo.easeInOut
+}, "-=5");
 
-const cards = document.querySelectorAll('.card');
-
-cards.addEventListener('hover', () => {
-    enlargeCards();
-});
-
-function enlargeCards(){
-    const cards = document.querySelectorAll('.card');
-    cards.forEach(card => {
-        card.classList.add('cardy');
-    });
-}
+screen.from(".native-full-screen", 3, {
+    delay: 0.2,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+}).from(".native-full-screen", 3, {
+    delay: 0.2,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+}, "-=5");
