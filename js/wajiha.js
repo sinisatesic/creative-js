66579231b5766a5019fea7c7059f49156eee9f7f
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-08-29 14:40:16 -0500

    slight updates to ccils

--- js/wajiha.js
+++ js/wajiha.js
@@ -38,36 +38,36 @@ var navbar = new TimelineMax();
 var screen = new TimelineMax();
 
 logo.from(".logo", 3, {
-    delay: 0.2,
+    delay: .1,
     opacity: 0,
-    x: -80,
+    x: -50,
     ease: Expo.easeInOut
 }).from(".logo", 3, {
-    delay: 0.2,
+    delay: .1,
     opacity: 0,
-    x: -80,
+    x: -50,
     ease: Expo.easeInOut
 }, "-=5");
 
 navbar.from(".navbar", 3, {
-    delay: 0.2,
+    delay: .1,
     opacity: 0,
     y: -40,
     ease: Expo.easeInOut
 }).from(".navbar", 3, {
-    delay: 0.2,
+    delay: .1,
     opacity: 0,
     y: -40,
     ease: Expo.easeInOut
 }, "-=5");
 
 screen.from(".native-full-screen", 3, {
-    delay: 0.2,
+    delay: .1,
     opacity: 0,
     y: 40,
     ease: Expo.easeInOut
 }).from(".native-full-screen", 3, {
-    delay: 0.2,
+    delay: .1,
     opacity: 0,
     y: 40,
     ease: Expo.easeInOut
