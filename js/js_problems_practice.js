26538140c08bcf88cf0b4067ad155f7f29dbb5d7
Sinisa Tesic <sinisa.tesic210@gmail.com>
2021-03-19 22:39:42 -0500

    js practice

--- js/js_problems_practice.js
+++ js/js_problems_practice.js
@@ -293,3 +293,70 @@ function secondLargest(arr) {
     return Math.max(...newArr)
 
 }
+
+
+//
+
+// Create a function that returns a base-2 (binary) representation of a base-10 (decimal) string number. To convert is simple: ((2) means base-2 and (10) means base-10) 010101001(2) = 1 + 8 + 32 + 128.
+
+// Going from right to left, the value of the most right bit is 1, now from that every bit to the left will be x2 the value, value of an 8 bit binary numbers are (256, 128, 64, 32, 16, 8, 4, 2, 1).
+
+// Examples
+// binary(1) ➞ "1"
+// // 1*1 = 1
+
+// binary(5) ➞ "101"
+// // 1*1 + 1*4 = 5
+
+// binary(10) ➞ "1010"
+// // 1*2 + 1*8 = 10
+// Notes
+// Numbers will always be below 1024 (not including 1024).
+// The && operator could be useful.
+// The strings will always go to the length at which the most left bit's value gets bigger than the number in decimal.
+// If a binary conversion for 0 is attempted, return "0".
+
+function binary(decimal) {
+    return decimal.toString(2);
+}
+
+// jeli:
+
+function binary(decimal) {
+    let result = "";
+    if (decimal == 0) {
+        result = "0";
+    }
+    while (decimal > 0) {
+        result += decimal % 2;
+        decimal = Math.floor(decimal/2);
+    };
+    return result.split("").reverse().join("");
+}
+
+
+////////////
+
+// Create a function to return the amount of potatoes there are in a string.
+
+// Examples
+// potatoes("potato") ➞ 1
+
+// potatoes("potatopotato") ➞ 2
+
+// potatoes("potatoapple") ➞ 1
+
+//
+
+const potatoes = s => s.match(/potato/g).length;
+
+// jeli:
+
+function potatoes(str) {
+    var arr = str.split("potato")
+    return arr.length-1
+}
+
+// jeli:
+
+potatoes = s => s.split("potato").length-1
